e90b9ecf2f13d72271588ab0a3277f14ab9a8f36
Chaitanya Vadrevu <chaitu236@tamu.edu>
2014-10-30 16:31:25 +0000

    After running rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Virtues13::Application.config.secret_token = 'c366a382f8e5bec7538d2e4edf6893b65c0bf0822c020e7b36736778da1c62b831819bf47a55d31292eec433282e5e7d6dca1d9f6bd8de0a901576a27310b179'
