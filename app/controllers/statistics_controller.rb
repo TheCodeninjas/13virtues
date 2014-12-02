12112ee2323c191391436802886a3b3077bc5702
Chaitanya Vadrevu <chaitu236@tamu.edu>
2014-12-02 23:07:53 +0000

    display student stats for only applicable virtues

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -6,10 +6,10 @@ class StatisticsController < ApplicationController
 		week = week.to_i
 
 #		@stats = [['year', 'sales', 'expenses'], ['2004', 1000, 400], ['2005', 1170, 460]]
-		@violations = Array.new(week+2) { Array.new(14, 0) }#[week][13]
+		@violations = Array.new(week+2) { Array.new(week+2, 0) }#[week][13]
 		@violations[0][0] = 'Week'
 
-		(1..13).to_a.each do|virt|
+		(1..week+1).to_a.each do|virt|
 			@violations[0][virt] = 'Virtue '+virt.to_s
 		end
 
@@ -26,7 +26,7 @@ class StatisticsController < ApplicationController
 			st_date = @classroom.startdate+(wk-1)*7
 			en_date = st_date+6
 
-			(1..13).to_a.each do|i|
+			(1..week+1).to_a.each do|i|
 				viols = @classregistration.virtueentries.find_all_by_v_id_and_date i-1, st_date..en_date
 				viols.each do |viol|
 					@violations[wk][i] += viol.violation_count
