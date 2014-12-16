173896f3620c92501729fd4dfc2b1e15262bd70c
Chaitanya Vadrevu <chaitu236@tamu.edu>
2014-12-16 00:58:26 +0000

    change root email to root, remove test user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,4 @@
-root = User.new :email=>'root@admin.com', :first_name=>'root'
+root = User.new :email=>'root', :first_name=>'root'
 root.user_type = User.admin_type
 root.password = "root"
 root.save!
-
-norm = User.new :email=>'test@testing.com', :first_name=>'test'
-norm.user_type = User.non_admin_type
-norm.password = "test"
-norm.save!
